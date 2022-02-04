5b0d59e4e3fae42e0b6050c9c49a5dd38240d54b
earlman <earl.mangulabnan@gmail.com>
2022-02-04 16:59:31 -0600

    only passthrough specific .woff2 files

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,18 @@
 module.exports = (eleventyConfig) => {
    eleventyConfig.addWatchTarget("./styles/");
 
-   //copy /images to dist/images
+   // copy /images to dist/images
    // eleventyConfig.addPassthroughCopy("./src/images");
 
    eleventyConfig.addPassthroughCopy({
-      "node_modules/@fontsource/open-sans/files": "css/fonts",
-      "node_modules/@fontsource/merriweather/files": "css/fonts",
+      // "node_modules/@fontsource/open-sans/files": "css/fonts",
+      // "node_modules/@fontsource/merriweather/files": "css/fonts",
+
+      // Only copying the specific .woff2 files I need because the hot-reloading is taking a while
+      "node_modules/@fontsource/open-sans/files/open-sans-latin-ext-variable-full-normal.woff2": "css/fonts/open-sans-latin-ext-variable-full-normal.woff2",
+      "node_modules/@fontsource/open-sans/files/open-sans-latin-variable-full-normal.woff2": "css/fonts/open-sans-latin-variable-full-normal.woff2",
+      "node_modules/@fontsource/merriweather/files/merriweather-latin-ext-400-normal.woff2": "css/fonts/merriweather-latin-ext-400-normal.woff2",
+      "node_modules/@fontsource/merriweather/files/merriweather-latin-400-normal.woff2": "css/fonts/merriweather-latin-400-normal.woff2",
    });
 
    return {

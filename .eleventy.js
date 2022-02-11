e3f138c3a4c82656ef6358f239f528ba6201c08e
earlman <earl.mangulabnan@gmail.com>
2022-02-11 13:30:18 -0600

    build: test slack notification

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,6 @@ module.exports = (eleventyConfig) => {
    eleventyConfig.addPassthroughCopy({
       // "node_modules/@fontsource/open-sans/files": "css/fonts",
       // "node_modules/@fontsource/merriweather/files": "css/fonts",
-
       // Only copying the specific .woff2 files I need because the hot-reloading is taking a while
       "node_modules/@fontsource/open-sans/files/open-sans-latin-ext-variable-full-normal.woff2": "css/fonts/open-sans-latin-ext-variable-full-normal.woff2",
       "node_modules/@fontsource/open-sans/files/open-sans-latin-variable-full-normal.woff2": "css/fonts/open-sans-latin-variable-full-normal.woff2",

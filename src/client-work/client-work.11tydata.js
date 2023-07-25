65774e54ec540b4096eb043f26031e7bfab5cd00
earlman <earl.mangulabnan@gmail.com>
2023-07-25 17:00:59 -0500

    fix data file

---
+++ src/client-work/client-work.11tydata.js
@@ -0,0 +1,31 @@
+//docs: https://github.com/octokit/core.js#readme
+const { Octokit } = require("@octokit/core");
+
+// Gets Github language data from api
+// Requires frontmatter to be set:
+// - repo.repo
+// - repo.owner
+async function getGithubData(frontmatterInput) {
+   const octokit = new Octokit({ auth: process.env.GITHUB_KEY });
+
+   if (!frontmatterInput) {
+      return null;
+   }
+
+   return octokit
+      .request("GET /repos/{owner}/{repo}/languages", {
+         owner: frontmatterInput.owner,
+         repo: frontmatterInput.repo,
+      })
+      .then((r) => {
+         return JSON.stringify(r.data);
+      });
+}
+
+module.exports = {
+   eleventyComputed: {
+      githubData: async ({ repo }) => await getGithubData(repo),
+   },
+   layout: "layouts/work-item.njk",
+   tags: "project",
+};

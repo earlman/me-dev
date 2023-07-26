0ba5cceda4fbf8a72b4258e5c9bd2f2e32e8e15e
earlman <earl.mangulabnan@gmail.com>
2023-07-26 03:36:55 -0500

    add 11tydata for each project

---
+++ src/client-work/bdi-global-llc.11tydata.js
@@ -0,0 +1,14 @@
+module.exports = function (data) {
+   return {
+      tech: [
+         {
+            name: "VueRouter",
+            link: "https://router.vuejs.org",
+         },
+         {
+            name: "VueRouter-2",
+            link: "https://router.vuejs.org",
+         },
+      ],
+   };
+};
